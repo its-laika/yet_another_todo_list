afc14bc84c7e79f7904ce8556ac38883df9587a2
Curly <36010519+neon-JS@users.noreply.github.com>
2024-07-28 20:02:25 +0200

    feat: Add frontend

---
+++ frontend/src/main.js
@@ -0,0 +1,75 @@
+let todos = [];
+
+function loadTodos() {
+    fetch(`${getOrigin()}/todos`)
+        .then(response => response.json())
+        .then(t => todos = t.filter(e => !e.done))
+        .then(_ => Handlebars.templates.table({ todos }))
+        .then(html => document.getElementById('table-container').innerHTML = html);
+}
+
+function addTodo(event) {
+    if (event) {
+        event.preventDefault();
+    }
+
+    const body = {
+        text: event.target[0].value,
+        done: false,
+    };
+
+    fetch(
+        `${getOrigin()}/todos`,
+        {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    )
+        .then(response => response.json())
+        .then(t => todos.push(t))
+        .then(_ => Handlebars.templates.table({ todos }))
+        .then(html => document.getElementById('table-container').innerHTML = html);
+}
+
+function updateTodo(todoId, done) {
+    const body = {
+        text: todos.find(t => t.id === todoId).text,
+        done
+    };
+
+    fetch(
+        `${getOrigin()}/todos/${todoId}`,
+        {
+            method: 'PUT',
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    )
+        .then(_ => todos.forEach(t => {
+            if (t.id === todoId) {
+                t.done = done;
+            }
+        }))
+        .then(_ => Handlebars.templates.table({ todos }))
+        .then(html => document.getElementById('table-container').innerHTML = html);
+}
+
+function deleteTodo(todoId) {
+    fetch(`${getOrigin()}/todos/${todoId}`, { method: 'DELETE' })
+        .then(_ => todos = todos.filter(t => t.id !== todoId))
+        .then(_ => Handlebars.templates.table({ todos }))
+        .then(html => document.getElementById('table-container').innerHTML = html);
+}
+
+/* Convenience function that allows us to globally use another origin for e.g. development */
+function getOrigin() {
+    return window.location;
+}
+
+document.querySelector('form').addEventListener('submit', addTodo);
+loadTodos();
